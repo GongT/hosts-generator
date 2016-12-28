fbb7c5e70291032a0d6473bd5b5a9edabb7bdca1
GongT <gongteng524702837@gmail.com>
2016-12-28 16:01:31 +0800

    add localhost-loop

--- src/boot.ts
+++ src/boot.ts
@@ -8,6 +8,7 @@ import {detectUpstream} from "./genterate/detect-upstream";
 import {runningDockerContainers} from "./genterate/docker-containers";
 import {serviceNotOnCurrent} from "./genterate/outside-services";
 import {mergeHosts} from "./lib/read-write";
+import {determineDockerInterfaceIpAddress} from "./genterate/get-interface";
 
 const debug = Debug('host:main');
 
@@ -15,6 +16,17 @@ handleChange((list) => {
 	debug('docker status changed!');
 	
 	const hostParts = [];
+	
+	try {
+		hostParts.push('\n# LOCAL INTERFACE: ');
+		const ips = determineDockerInterfaceIpAddress();
+		hostParts.push(`${ips[0]}  localhost-loop`);
+		if (ips[1]) {
+			hostParts.push(`${ips[1]}  localhost-loop6`);
+		}
+	} catch (e) {
+		hostParts.push(`## ERROR. ${JSON.stringify(e.message)}`);
+	}
 	
 	hostParts.push('\n# PHYSICAL HOST: ');
 	hostParts.push(generateIdIpMap());
